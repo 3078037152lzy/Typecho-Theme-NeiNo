bea0549e8fbf573fb952e504f2ae27693be289d9
obentnet <1283224552@qq.com>
2019-07-28 02:06:28 +0800

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,8 @@
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <div class="sidebar">
                 <nobr>
                     <a href="<?php $this->options->siteUrl(); ?>"><button type="button" class="layui-btn layui-btn-radius layui-btn-normal">首页</button></a>
-                    <a href="<?php $this->options->siteUrl(); ?>"><button type="button" class="layui-btn layui-btn-radius layui-btn-normal">首页</button></a>
+                    <?php $this->widget('Widget_Contents_Page_List')
+                                ->parse('<a href="{permalink}"><button type="button" class="layui-btn layui-btn-radius layui-btn-normal">{title}</button></a>&nbsp;'); ?>
                 </nobr>
             </div>
