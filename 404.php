76d8599cfb78d01bb9db92bf45a03243a24919a3
obentnet <1283224552@qq.com>
2019-07-28 02:05:30 +0800

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,31 @@
+<!--
+    看着很眼熟是吧！
+    没错，偷了B站的一个图QWQ，希望不会被打吧2333
+    By NeiNo Theme writer
+-->
+<html>
+    <head>
+        <title>Opps!404!</title>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" href="https://static.hdslb.com/error/dist/error.css">
+        <link rel="stylesheet" href="//cdnjs.loli.net/ajax/libs/mdui/0.4.3/css/mdui.min.css">
+        <script src="//cdnjs.loli.net/ajax/libs/mdui/0.4.3/js/mdui.min.js"></script>
+        <style>
+            .main{
+                text-align:center;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="main">
+            <img src="//static.hdslb.com/error/very_sorry.png">
+            <div class="main-bottom">
+                <a href="<?php $this->options->siteUrl(); ?>"><button class="mdui-btn mdui-color-blue-200 mdui-ripple">返回首页</button></a>
+            </div>
+            <br>
+            <br>
+            <div class="mdui-typo-caption">&copy;</div>
+            <div class="mdui-typo-caption">image for Bilibili design by mdui 1Jnver</div>
+        </div>
+    </body>
+</html>
