2ff3fd2c7b51bde6eb42150334b7335029a75af7
obentnet <1283224552@qq.com>
2019-07-28 00:17:48 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,12 @@
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+            <div class="footer">
+                <nobr>
+                    &copy;2019 <?php $this->options->title() ?> <br>
+                    Powered By <a href="http://typecho.org" target="_blank">Typecho</a> |
+                    Theme By NeiNo
+                </nobr>
+            </div>
+            <br>
+        </div>
+    </body>
+</html>
