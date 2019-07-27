021e01212fb80abced4b0eace4480522b6b64b94
obentnet <1283224552@qq.com>
2019-07-28 00:18:42 +0800

    Create sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,6 @@
+<div class="sidebar">
+                <nobr>
+                    <a href="<?php $this->options->siteUrl(); ?>"><button type="button" class="layui-btn layui-btn-radius layui-btn-normal">首页</button></a>
+                    <a href="<?php $this->options->siteUrl(); ?>"><button type="button" class="layui-btn layui-btn-radius layui-btn-normal">首页</button></a>
+                </nobr>
+            </div>
