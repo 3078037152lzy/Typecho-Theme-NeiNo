bc545b387712758b0c13de7777df5fdf3a180bf2
obentnet <1283224552@qq.com>
2019-07-28 02:05:40 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,9 +1,11 @@
-<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+include('neino.php')
+?>
             <div class="footer">
                 <nobr>
-                    &copy;2019 <?php $this->options->title() ?> <br>
-                    Powered By <a href="http://typecho.org" target="_blank">Typecho</a> |
-                    Theme By NeiNo
+                    &copy;<?php $this->date('Y'); ?><?php $this->options->title() ?> <br>
+                    Powered By <a href="http://typecho.org" target="_blank">Typecho</a> |<?php /**版权输出 */ "echo Theme By NeiNo"?>
+                    <?php echo $neino[0]?>
                 </nobr>
             </div>
             <br>
