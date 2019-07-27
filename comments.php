d6430c4824d4d85e230eb3a44f3d384eb88da9b5
obentnet <1283224552@qq.com>
2019-07-28 00:22:49 +0800

    Update comments.php

--- comments.php
+++ comments.php
@@ -1,78 +1,101 @@
-<?php if(!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
-
-<?php
-function threadedComments($comments, $options) {
-    $commentClass = '';
-	if($comments -> authorId == $comments -> ownerId){
-		$commentClass .= 'comment-by-author';
-	}
-	else{
-		$commentClass .= 'comment-by-user';
-	}
-    $commentLevelClass = $comments -> levels > 0 ? ' comment-child' : '';
-?>
-
-<div class="comments <?php echo $commentLevelClass; ?>" id="<?php $comments -> theId() ?>">
-    <?php $comments -> gravatar('150', 'robohash'); ?>
-    <div class="comment-meta">
-        <span class="comment-author"><?php if($comments -> url): ?><a href="<?php $comments -> url() ?>" rel="external nofollow" target="_blank"><?php $comments->author(false) ?></a><?php else: $comments->author(); endif; ?></span>
-        <time class="comment-time"><?php $comments -> created(); ?></time>
-        <span class="comment-reply"><?php $comments -> reply('<i class="fa fa-reply" title="回复"></i>'); ?></span>
-    </div>
-    <div class="comment-content">
-        <p>
-<?php
-if($comments -> parent){
-    echo '<a href="#comment-' . $comments -> parent . '">@</a> ';
-}
-$comments -> content = preg_replace('#</?[p][^>]*>#','', $comments -> content);
-$comments -> content();
-?>
-        </p>
-    </div>
-</div>
-
-<?php if($comments -> children) $comments -> threadedComments(); ?>
-
-<?php } ?>
-
-<section class="comments-input">
-    <h3><?php $this -> commentsNum(_t('没有评论'), _t('只有一条评论 (QwQ)'), _t('已有 %d 条评论')); ?></h3>
-<?php $this -> comments() -> to($comments); ?>
-<?php if($this -> allow('comment')): ?>
-    <div class="comment-form" id="<?php $this -> respondId(); ?>">
-        <span class="cancel-comment-reply">
-            <?php $comments -> cancelReply(); ?>
-        </span>
-        <form method="post" action="<?php $this -> commentUrl() ?>" role="form">
-            <?php if($this -> user -> hasLogin()): ?>
-                <fieldset>
-                    <p>欢迎回来，<a href="<?php $this -> options -> profileUrl() ?>"><?php $this -> user -> screenName(); ?></a>！不是你？<a href="<?php $this -> options -> logoutUrl() ?>">登出</a></p>
-                    <textarea rows="2" name="text" id="textarea" placeholder="快来评论吧 (*≧ω≦)ﾉ" required><?php $this -> remember('text'); ?></textarea>
-                    <button type="submit" class="btn">写好了~</button>
-                </fieldset>
-            <?php else: ?>
-            <div class="row">
-                <fieldset class="col-m-6">
-                    <input type="text" name="author" placeholder="昵称 *：" value="<?php $this -> remember('author'); ?>" required>
-                    <input type="email" name="mail" placeholder="电邮 *：" value="<?php $this -> remember('mail'); ?>"<?php if($this -> options -> commentsRequireMail): ?> required<?php endif; ?>>
-                    <input type="url" name="url" placeholder="http://" value="<?php $this -> remember('url'); ?>"<?php if($this -> options -> commentsRequireURL): ?> required<?php endif; ?>>
-                </fieldset>
-                <fieldset class="col-m-6">
-                    <textarea rows="3" name="text" id="textarea" placeholder="快来评论吧 (*≧ω≦)ﾉ" required><?php $this -> remember('text'); ?></textarea>
-                    <button type="submit" class="btn">写好了~</button>
-                </fieldset>
+<!--求助啊啊啊啊-->
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=2.0, user-scalable=no" />
+        <title>Typecho NeiNo</title>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" href="./style.css">
+        <link rel="stylesheet" href="./layui/css/layui.css"/>
+        <link rel="stylesheet" href="./layui/css/layui.mobile.css"/>
+        <script src="layui/layui.all.js"></script>      
+    </head>
+    <body>
+        <div class="main">
+            <div class="sidebar">
+                <nobr>
+                    <a href="SIteURL"><button type="button" class="layui-btn layui-btn-radius layui-btn-normal">首页</button></a>
+                </nobr>
             </div>
-<?php endif; ?>
-        </form>
-    </div>
-<?php else: ?>
-        <p>博主关闭了评论...</p>
-<?php endif; ?>
+            <!--Main-->
+            <div class="post-main">
+                <div class="post-main-main">
+                    <div class="post-main-title">
+                        <font size="6px;">习近平访华</font>
+                    </div>
+                    <info>#默认分类 | 2019-7-25 | 0条评论</info>
+                    <br>
+                    <br>
+                    <br>
+                    <font size="4px">
+                            　作风问题关系人心向背，关系党的执政基础。
 
-<?php if($comments -> have()): ?>
-        <div class="comment-list"><?php $comments -> listComments(array('before' => '', 'after' => '', 'replyWord' => '<i class="fa fa-reply"></i>')); ?></div>
-        <?php $comments -> pageNav('&laquo;', '&raquo;'); ?>
-<?php endif; ?>
+                            　2016年10月，习近平在党的十八届六中全会第二次全体会议上强调：<br>
+                        
+                            　“如果党内信念涣散、组织涣散、纪律涣散、作风涣散，那就无法有效应对党面临的执政考验、改革开放考验、市场经济考验、外部环境考验，也无法克服精神懈怠危险、能力不足危险、脱离群众危险、消极腐败危险，最终不仅不能实现我们的奋斗目标，而且可能严重脱离人民群众，上演霸王别姬的悲剧。这就是党中央反复强调‘打铁还需自身硬’的根本原因。”<br>
+                        
+                            　这并不是习近平第一次提及“霸王别姬”的故事。2013年7月，习近平在河北调研指导党的群众路线教育实践活动时说：“我们党的执政基础很牢固，但如果作风问题解决不好，也有可能出现‘霸王别姬’这样的时刻。我们一定要有危机意识。”<br>
+                        
+                            　“霸王别姬”的故事始见于《史记·项羽本纪》，讲的是西楚霸王项羽在与汉王刘邦争夺天下中兵败失势，溃败之际与他宠爱的美人虞姬诀别的故事。<br>
+                        
+                            　2000多年来，“霸王别姬”被用来形容英雄末路的悲壮情景。现多比喻骄傲自满，独断专行，脱离群众，最终导致失败。<br>
+                        
+                            　“我们一定要有危机意识。”习近平用“霸王别姬”的故事警示全党：党的作风就是党的形象，关系人心向背，关系党的生死存亡。这一警示，是出于对执政党命运的深刻思考，值得每个共产党人自律和警醒。<br>
+                        
+                            　党的十八大以来，以习近平同志为核心的党中央敢于刀刃向内，勇于自我革命，以整治“四风”为突破口，着力解决党内存在的突出问题，以雷霆万钧之力反腐败，刹住了一些过去被认为不容易刹住的歪风邪气，克服了一些司空见惯的顽瘴痼疾，党风政风明显好转。但是，“还要看到，‘四风’问题树倒根存，形式主义、官僚主义问题依然突出。”2019年5月31日，习近平在“不忘初心、牢记使命”主题教育工作会议上语重心长地指出。<br>
+                        
+                            　他强调，开展这次主题教育要“奔着问题去，以刮骨疗伤的勇气、坚忍不拔的韧劲坚决予以整治，同一切影响党的先进性、弱化党的纯洁性的问题作坚决斗争”。<br>
+                        
+                            　自我革命永不停步，全面从严治党永远在路上，作风建设永远在路上，是习近平反复提及“霸王别姬”故事的深意所在。<br>
+                            <img src="https://gss1.bdstatic.com/-vo3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike80%2C5%2C5%2C80%2C26/sign=468864cdf0039245b5b8e95de6fdcfa7/54fbb2fb43166d22ca0c87944a2309f79052d2b3.jpg" alt="">
+                    </font>
+                </div>
+            </div>
+            <!--Comments-->
 
-</section>
+            <div class="comments">
+                <div class="comments-main">
+                    <h2>Comments</h2>
+                    <div class="comments-one">
+                        <div class="comments-one-head">
+                            <img src="https://yujienb.cn/head.jpg" alt=""/>
+                        </div>
+                        <div class="comments-one-name">
+                            <font size="3px;">狱杰</font>
+                            &nbsp;&nbsp;&nbsp;<a href="">回复</a>
+                        </div>
+                        <div class="comments-one-say">
+                            牛逼
+                        </div>
+                        <br>
+                    </div>
+                </div>
+            </div>
+
+            <div class="comments-input">
+                <div class="comments-input-main">
+                    <h2>留下你想说的</h2>
+                    <div class="comments-input-input">
+                        <form action="">
+                                <input type="text" name="title"  placeholder="昵称" autocomplete="off" class="inputstyle1">
+                                <input type="text" name="title"  placeholder="邮箱" autocomplete="off" class="inputstyle1">
+                                <input type="text" name="title"  placeholder="网址" autocomplete="off" class="inputstyle1">
+                                <br>
+                                <input type="text" name="title"  placeholder="留言" autocomplete="off" class="inputstyle2">
+                                <input type="submit" value="Fa♂送留言" class="layui-btn layui-btn-radius layui-btn-normal" /></p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <!--comments-over-->
+            <!--Footer-->
+            <div class="footer">
+                    <nobr>
+                        &copy;2019 SITENAME <br>
+                        Powered By <a href="http://typecho.org" target="_blank">Typecho</a> |
+                        Theme By NeiNo
+                    </nobr>
+                </div>
+                <br>
+        </div>
+    </body>
+</html>
