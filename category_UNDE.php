e8ecd0dc48cea60a4a2113e7b14c0aac048c4215
obentnet <1283224552@qq.com>
2019-07-28 15:51:27 +0800

    Create category_UNDE.php

---
+++ category_UNDE.php
@@ -0,0 +1,16 @@
+<?if (!defined('__TYPECHO_ROOT_DIR__')) exit;?>
+<?php $this->need('header.php'); ?>
+<body>
+    <div class="main">
+        <?php $this->need('sidebar.php'); ?>
+        <div class="category">
+            <div class="category-title"><h2>#默认分类 下的文章</h2></div>
+            <div class="category-main">
+                <li>
+                    <a href="">123123</a>
+                </li>
+            </div>
+        </div>
+        <?php $this->need('footer.php'); ?>
+    </div>
+</body>
